b4ecb745cac7436b616cc3687984342779a4275f
karthikbc <karthikeyan@braincert.com>
2025-03-23 13:01:20 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+    import express from 'express';
+    import cors from 'cors'
+    import 'dotenv/config'
+    import connectDB from './mongodb.js';
+    import {clerkWebhooks} from './controllers/webhooks.js'
+
+    const app = express();
+
+    await connectDB();
+    
+    app.use(cors());
+
+    app.get('/', (req,res) => res.send("api working"));
+    app.post('/clerk', express.json(), clerkWebhooks)
+
+    const PORT = process.env.PORT || 5000
+
+    app.listen(PORT, () => {
+        console.log(`server is running port ${PORT}`)
+    }) 
